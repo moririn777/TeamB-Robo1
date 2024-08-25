704aed97121b8d157767873810b8e26de923aa2d
moririn <moriya8229@gmail.com>
2024-08-25 23:35:55 +0900

    feat: :zap: メカナムの各RPMをCANで送信する

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,33 @@ unsigned int ID = 0x555; //ID
 unsigned long long data;
 
 uint8_t canData[8];
+
 struct Motor_RPMs{
-  int16_t frontLeft;
-  int16_t frontRight;
-  int16_t rearLeft;
-  int16_t rearRight;
+  uint16_t frontLeft;
+  uint16_t frontRight;
+  uint16_t rearLeft;
+  uint16_t rearRight;
 };
 
 unsigned long long combineMotorRPMs(Motor_RPMs RPMs) {
     unsigned long long  RPMdata = 0;
     
     // 各モーターの RPM 値をシフトして結合（16ビットに制限）
-    RPMdata |= ((unsigned long long)(RPMs.frontLeft & 0xFFFF) << 48);
-    RPMdata |= ((unsigned long long)(RPMs.frontRight & 0xFFFF) << 32);
-    RPMdata |= ((unsigned long long)(RPMs.rearLeft & 0xFFFF) << 16);
-    RPMdata |= (unsigned long long)(RPMs.rearRight & 0xFFFF);
-    
+    RPMdata |= ((unsigned long long)((uint16_t)RPMs.frontLeft) << 48);
+    RPMdata |= ((unsigned long long)((uint16_t)RPMs.frontRight) << 32);
+    RPMdata |= ((unsigned long long)((uint16_t)RPMs.rearLeft) << 16);
+    RPMdata |= (unsigned long long)((uint16_t)RPMs.rearRight);
+
     return RPMdata;
 }
 
 Motor_RPMs calculateWheelRPMs(int x,int y, int rotation){//メカナムの計算
   Motor_RPMs RPMs;
-
-  if(abs(x) < dead_zone && abs(y) < dead_zone && abs(rotation)){
-    RPMs.frontLeft = 00000000;
-    RPMs.frontRight = 00000000;
-    RPMs.rearLeft = 00000000;
-    RPMs.rearRight = 00000000;
-  }else{
-    RPMs.frontLeft = -x + y + (WHEELBASE_X + WHEELBASE_Y)*rotation;
-    RPMs.frontRight = x + y + (WHEELBASE_X + WHEELBASE_Y)*rotation;
-    RPMs.rearLeft = -x - y + (WHEELBASE_X + WHEELBASE_Y)*rotation;
-    RPMs.rearRight = x - y + (WHEELBASE_X + WHEELBASE_Y)*rotation;
-  }
+  
+  RPMs.frontLeft = (int16_t)(-x + y + (WHEELBASE_X + WHEELBASE_Y)*rotation);
+  RPMs.frontRight = (int16_t)(x + y + (WHEELBASE_X + WHEELBASE_Y)*rotation);
+  RPMs.rearLeft = (int16_t)(-x - y + (WHEELBASE_X + WHEELBASE_Y)*rotation);
+  RPMs.rearRight = (int16_t)(x - y + (WHEELBASE_X + WHEELBASE_Y)*rotation);
   return RPMs;
 }
 
@@ -52,45 +46,44 @@ void setup() {
     Serial.println("ERROR:Starting CAN failed!");
     while (1);
   }
-  PS4.begin("00:00:00:00:00:00");
+  
+  PS4.begin("48:e7:29:a3:c5:0c");
 }
 
 void loop() {
   
   if(!PS4.isConnected()){
-    Serial.println("ERROR:Cant Connect!!");
+    Serial.println("ERROR:Cant PS4Connect!!");
     return;
   }
 
-  int right_x = PS4.RStickX();
-  int right_y = PS4.RStickY();
   int left_x = PS4.LStickX();
+  int left_y = PS4.LStickY();
+  int right_x = PS4.RStickX();
   //int left_y = PS4.LStickY();
-
-  Motor_RPMs RPMs = calculateWheelRPMs(right_x,right_y,left_x);
-  //Motor_RPMs RPMs = calculateWheelRPMs(0,0,0);
-  data = combineMotorRPMs(RPMs);
-
-  Serial.printf("FL::%d\r\n",int(RPMs.frontLeft));
-  Serial.printf("FR::%d\r\n",int(RPMs.frontRight));
-  Serial.printf("RL::%d\r\n",int(RPMs.rearLeft));
-  Serial.printf("RR::%d\r\n",int(RPMs.rearRight));
-  delay(1);
   
-  CAN.beginPacket(ID);
-  CAN.write(canData, sizeof(canData));  
-  CAN.endPacket();  
-  /*
+  Motor_RPMs RPMs;
+
+  if(abs(left_x) < dead_zone && abs(left_y) < dead_zone && abs(right_x)<dead_zone){
+    data = 0;
+  } else{
+    RPMs = calculateWheelRPMs(left_x,left_y,right_x);
+    data = combineMotorRPMs(RPMs);
+  }
+
+  Serial.printf("data: 0x%016llX\n", data);
+  Serial.printf("FL::%x\r\n",uint16_t(RPMs.frontLeft));
+  Serial.printf("FR::%X\r\n",uint16_t(RPMs.frontRight));
+  Serial.printf("RL::%X\r\n",uint16_t(RPMs.rearLeft));
+  Serial.printf("RR::%X\r\n",uint16_t(RPMs.rearRight));
+  
   for (int i = 0; i < 8; i++) {
     canData[i] = (data >> (56 - i * 8)) & 0xFF;
   }
-  for (int i = 0; i < 8; i +=2) {
-    Serial.printf("%02X%02X ", canData[i],canData[i+1]);
-  }
-    Serial.println();
-  */
 
+  CAN.beginPacket(ID);
+  CAN.write(canData, sizeof(canData));  
+  CAN.endPacket();  
+  
+  delay(10);
 }
-
-
-
