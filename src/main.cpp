29f524849deca3abdedf61056be9fbf30808cfbb
moririn <moriya8229@gmail.com>
2024-09-18 23:50:53 +0900

    fix:RPMsをuint8_tの配列にキャストした

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ const int WHEELBASE_Y = 1;
 const int DEAD_ZONE = 30;
 
 const unsigned int ID = 0x555; // ID
-unsigned long long data;
 
 uint8_t canData[8];
 
@@ -17,18 +16,6 @@ struct Motor_RPMs {
   int16_t rearLeft;
   int16_t rearRight;
 };
-
-unsigned long long combineMotorRPMs(Motor_RPMs RPMs) {
-  unsigned long long RPMdata = 0;
-
-  // 各モーターの RPM 値をシフトして結合（16ビットに制限）
-  RPMdata |= ((unsigned long long)((uint16_t)RPMs.frontLeft) << 48);
-  RPMdata |= ((unsigned long long)((uint16_t)RPMs.frontRight) << 32);
-  RPMdata |= ((unsigned long long)((uint16_t)RPMs.rearLeft) << 16);
-  RPMdata |= (unsigned long long)((uint16_t)RPMs.rearRight);
-
-  return RPMdata;
-}
 
 Motor_RPMs calculateWheelRPMs(int x, int y, int rotation) { // メカナムの計算
   Motor_RPMs RPMs;
@@ -62,7 +49,7 @@ void loop() {
   int left_y = PS4.LStickY();
   int right_x = PS4.RStickX();
   // int left_y = PS4.LStickY();
-  
+
   if (abs(left_x) < DEAD_ZONE) {
     left_x = 0;
   }
@@ -76,20 +63,14 @@ void loop() {
   Motor_RPMs RPMs;
 
   RPMs = calculateWheelRPMs(left_x, left_y, right_x);
-  data = combineMotorRPMs(RPMs);
 
-  Serial.printf("data: 0x%016llX\r\n", data);
   Serial.printf("FL::%x\r\n", uint16_t(RPMs.frontLeft));
   Serial.printf("FR::%X\r\n", uint16_t(RPMs.frontRight));
   Serial.printf("RL::%X\r\n", uint16_t(RPMs.rearLeft));
   Serial.printf("RR::%X\r\n", uint16_t(RPMs.rearRight));
 
-  for (int i = 0; i < 8; i++) {
-    canData[i] = (data >> (56 - i * 8)) & 0xFF;
-  }
-
   CAN.beginPacket(ID);
-  CAN.write(canData, sizeof(canData));
+  CAN.write((uint8_t *)&RPMs, sizeof(RPMs));
   CAN.endPacket();
 
   delay(10);
