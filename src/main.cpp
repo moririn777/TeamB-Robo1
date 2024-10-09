82488c76536521f0ac97d868522bdeb7d74e6b7d
moririn <moriya8229@gmail.com>
2024-10-09 19:36:01 +0900

    エンコーダーの値をかえってこらせるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,45 @@
 #include <Arduino.h>
 #include <CAN.h>
-#include <ESP32Servo.h>
-#include <PS4Controller.h>
+#include <string.h>
 
 const int32_t WHEELBASE_X = 1;
 const int32_t WHEELBASE_Y = 1;
 const int32_t DEAD_ZONE = 30;
-const uint8_t LAUNCHING_SERVO_PIN = 32;
-const uint32_t ID = 0x555; // ID
-const int32_t SET_DEGREE = 0;      // 装填角度
-const int32_t LAUNCH_DEGREE = 45;  // 発射角度
-const int32_t DEBOUNCE_DELAY = 50; // チャタリング防止
 
-bool circle_pressed = false;
-uint32_t circle_debounce_time = 0;
+const uint32_t TX_ID = 0x100; // TX_ID
+
+const uint32_t RX_ID1 = 0x111; 
+const uint32_t RX_ID2 = 0x222;
+
 uint8_t can_data[8];
-Servo launchingServo;
-bool launch_flag;
+uint8_t rpm[2];
+
+unsigned long previous_millis = 0;
+
+float front_left_rpm = 0;
+float front_right_rpm = 0;
+float rear_left_rpm = 0;
+float rear_right_rpm = 0;
 
 class MotorRpms {
-  public:
-    MotorRpms(
-      int16_t front_left,
-      int16_t front_right,
-      int16_t rear_left,
-      int16_t rear_right
-    ) {
-      this->front_left = front_left;
-      this->front_right = front_right;
-      this->rear_left = rear_left;
-      this->rear_right = rear_right;
-    }
-    int16_t frontLeft() {
-      return this->front_left;
-    }
-    int16_t frontRight() {
-      return this->front_right;
-    }
-    int16_t rearLeft() {
-      return this->rear_left;
-    }
-    int16_t rearRight() {
-      return this->rear_right;
-    }
-  private:
-    int16_t front_left;
-    int16_t front_right;
-    int16_t rear_left;
-    int16_t rear_right;
+public:
+  MotorRpms(int16_t front_left, int16_t front_right, int16_t rear_left,
+            int16_t rear_right) {
+    this->front_left = front_left;
+    this->front_right = front_right;
+    this->rear_left = rear_left;
+    this->rear_right = rear_right;
+  }
+  int16_t frontLeft() { return this->front_left; }
+  int16_t frontRight() { return this->front_right; }
+  int16_t rearLeft() { return this->rear_left; }
+  int16_t rearRight() { return this->rear_right; }
+
+private:
+  int16_t front_left;
+  int16_t front_right;
+  int16_t rear_left;
+  int16_t rear_right;
 };
 
 uint64_t combineMotorRpms(MotorRpms *rpms) {
@@ -62,7 +54,8 @@ uint64_t combineMotorRpms(MotorRpms *rpms) {
   return rpm_data;
 }
 
-MotorRpms calculateWheelRpms(int8_t x, int8_t y, int8_t rotation) { // メカナムの計算
+MotorRpms calculateWheelRpms(int8_t x, int8_t y,
+                             int8_t rotation) { // メカナムの計算
   int16_t front_left = -x + y + (WHEELBASE_X + WHEELBASE_Y) * rotation;
   int16_t front_right = x + y + (WHEELBASE_X + WHEELBASE_Y) * rotation;
   int16_t rear_left = -x - y + (WHEELBASE_X + WHEELBASE_Y) * rotation;
@@ -70,69 +63,72 @@ MotorRpms calculateWheelRpms(int8_t x, int8_t y, int8_t rotation) { // メカナ
   return MotorRpms(front_left, front_right, rear_left, rear_right);
 }
 
-void setup() {
-  launchingServo.attach(LAUNCHING_SERVO_PIN);
-  launchingServo.write(0);
-  launch_flag = false; // サーボのロック状態
+void receiveFloatViaCan(uint8_t* rx_data, float* value) {
+    memcpy(value, rx_data, sizeof(float));  // RxDataからfloat型へコピー
+}
 
+void setup() {
   Serial.begin(115200);
   if (!CAN.begin(1000E3)) {
     Serial.println("ERROR:Starting CAN failed!");
   }
-  PS4.begin("08:B6:1F:ED:5E:34");
 }
 
 void loop() {
-
-  if (!PS4.isConnected()) {
-    Serial.println("ERROR:Cant PS4Connect!!");
-    return;
-  }
-
-  if (PS4.Circle()) {
-    if (!circle_pressed && millis() - circle_debounce_time > DEBOUNCE_DELAY) {
-      if (!launch_flag) {
-        launchingServo.write(LAUNCH_DEGREE);
-      } else {
-        launchingServo.write(SET_DEGREE);
-      }
-      launch_flag = !launch_flag;
-      circle_debounce_time = millis();
-    }
-    circle_pressed = true;
-  } else {
-    circle_pressed = false;
-  }
-
-  int8_t left_x = PS4.LStickX();
-  int8_t left_y = PS4.LStickY();
-  int8_t right_x = PS4.RStickX();
-  // int8_t left_y = PS4.LStickY();
-  
-  if (abs(left_x) < DEAD_ZONE) {
-    left_x = 0;
-  }
-  if (abs(left_y) < DEAD_ZONE) {
-    left_y = 0;
-  }
-  if (abs(right_x) < DEAD_ZONE) {
-    right_x = 0;
-  }
-  
-  MotorRpms rpms = calculateWheelRpms(left_x, left_y, right_x);
+  MotorRpms rpms = calculateWheelRpms(0, 0, 127);
   uint64_t data = combineMotorRpms(&rpms);
 
+  const long interval = 10; // 10ミリ秒
+  unsigned long currentMillis = millis();
+  
+  /*
   Serial.printf("data: 0x%016llX\r\n", data);
-  Serial.printf("FL::%x\r\n", uint16_t(rpms.frontLeft()));
-  Serial.printf("FR::%X\r\n", uint16_t(rpms.frontRight()));
-  Serial.printf("RL::%X\r\n", uint16_t(rpms.rearLeft()));
-  Serial.printf("RR::%X\r\n", uint16_t(rpms.rearRight()));
+  Serial.printf("FL::%d\r\n", uint16_t(rpms.frontLeft()));
+  Serial.printf("FR::%d\r\n", uint16_t(rpms.frontRight()));
+  Serial.printf("RL::%d\r\n", uint16_t(rpms.rearLeft()));
+  Serial.printf("RR::%d\r\n", uint16_t(rpms.rearRight()));
+  */
 
   for (int i = 0; i < 8; i++) {
     can_data[i] = (data >> (56 - i * 8)) & 0xFF;
   }
 
-  CAN.beginPacket(ID);
-  CAN.write(can_data, sizeof(can_data));
-  CAN.endPacket();
+  if (currentMillis - previous_millis >= interval) {
+    previous_millis = currentMillis;
+
+    CAN.beginPacket(TX_ID);
+    CAN.write(can_data, sizeof(can_data));
+    CAN.endPacket();
+  }
+
+  char buf[256];
+  int packetSize = CAN.parsePacket();
+
+  if (packetSize) {
+    // バッファに受信データを格納
+    byte received_data[8]; // 最大8バイトのデータ格納用
+    uint32_t receivedId = CAN.packetId(); // 受信したメッセージのIDを取得
+    for (int i = 0; i < packetSize; i++) {
+      received_data[i] = CAN.read(); // 受信したバイトを配列に格納
+    }
+    float received_rotations1, received_rotations2;
+
+    receiveFloatViaCan(received_data, &received_rotations1);
+    receiveFloatViaCan(received_data + 4, &received_rotations2);
+    Serial.print("Data: \r\n");
+    Serial.printf("rotations1: %f\r\n",received_rotations1);
+    Serial.printf("rotations2: %f\r\n",received_rotations2);
+    Serial.println();
+
+    if(receivedId == RX_ID1){
+      Serial.println("Processing data for ID1...");
+      front_left_rpm = received_rotations1;
+      front_right_rpm = received_rotations2;
+    }
+    if(receivedId == RX_ID2){
+      Serial.println("Processing data for ID2...");
+      rear_left_rpm = received_rotations1;
+      rear_right_rpm = received_rotations2;
+    }
+  }
 }
